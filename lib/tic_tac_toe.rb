476b614ee276e9eb207e264546fbb0d78bc7412b
Marvin Reyes <reyemarv@gmail.com>
2020-02-19 04:07:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,9 +100,9 @@ class TicTacToe
     def winner
             WIN_COMBINATIONS.detect do |combination|
         if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X"
-          then "X"
+          puts "X"
         else if @board[combination [0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O"
-            then "O"
+          puts "O"
           else nil 
           end 
         end 
