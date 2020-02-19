b8243ea5fc1a4aee197ebfb8ed9701974ed6f9cc
Marvin Reyes <reyemarv@gmail.com>
2020-02-19 02:32:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,8 @@ class TicTacToe
     
     def turn_count(board)
       turns = 0 
-      board.each {|i| if i == "X" || i == "O" 
+      board.each {|i| if i == "X" || "O" 
         turns += 1}
+      end 
     end 
 end 
