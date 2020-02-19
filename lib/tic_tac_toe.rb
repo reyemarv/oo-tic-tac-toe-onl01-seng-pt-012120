c0cfc862a42e920b6c25ebee0a8f9b6923a3980f
Marvin Reyes <reyemarv@gmail.com>
2020-02-19 03:38:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,9 +61,9 @@ class TicTacToe
     
     def turn(input)
       puts "Please enter 1-9:"
-      i = input_to_index(input)
-      if valid_move?(i)
-        move(i, current_player)
+      index = input_to_index(input)
+      if valid_move?(index)
+        move(index, current_player)
         display_board
       else 
         puts "Please enter 1-9:"
