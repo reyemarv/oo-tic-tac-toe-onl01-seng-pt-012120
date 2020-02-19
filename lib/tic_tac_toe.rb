22426af9f383a363cb6e2cbae07eeeae4b2f55e9
Marvin Reyes <reyemarv@gmail.com>
2020-02-19 05:23:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -104,9 +104,15 @@ class TicTacToe
     end 
     
     def play
-     until over?
-    end 
-  end
+       until over?
+        turn
+      end 
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end 
+    end
         
 end 
 
