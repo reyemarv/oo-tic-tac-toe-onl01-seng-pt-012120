42afa391cc958826c43265708f096f65fefd62e6
Marvin Reyes <reyemarv@gmail.com>
2020-02-19 02:15:56 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,44 @@
+class TicTacToe
+
+    WIN_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [1, 4, 7],
+        [0, 3, 6],
+        [2, 5, 8]
+    ]
+
+    def initialize(board = nil)
+        @board = board || Array.new(9, " ")
+    end 
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end 
+    
+    def input_to_index(input)
+      input.to_i - 1  
+    end 
+    
+    def move(index, token = "X")
+      @board[index] = token
+    end 
+    
+    def position_taken?(index)
+      if @board[index] == " "
+        false 
+      else true 
+      end 
+    end 
+    
+    def valid_move?(index)
+     index.between?(0,8) && !position_taken?(index)
+    end 
+end 
